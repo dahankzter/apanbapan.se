c7252c0549f2ea7a338978f5834f5a30eddf3c28
Henrik Johansson <dahankzter@gmail.com>
2018-04-23 11:41:54 +0200

    Simpler deploy

--- magefile.go
+++ magefile.go
@@ -21,11 +21,7 @@ func Clean() error {
 func Deploy() error {
 	mg.Deps(Build)
 
-	if err := sh.Run("scp", "-r", "public", "dahankzter@apanbapan.se:."); err != nil {
-		return err
-	}
-
-	if err := sh.Run("ssh", "dahankzter@apanbapan.se", "cp ", "-r", "public/*", "/www/www.apanbapan.se/."); err != nil {
+	if err := sh.Run("rsync", "-avz", "--delete", "public/", "dahankzter@apanbapan.se:/www/www.apanbapan.se/"); err != nil {
 		return err
 	}
 
